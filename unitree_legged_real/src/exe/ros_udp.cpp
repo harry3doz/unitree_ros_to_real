e7d09da97129224746248784b453aa7bc92d7b50
harry3doz <k.hiroyuki.perc@gmail.com>
2023-05-09 08:35:30 +0000

    publish imu and odometry, wip

--- unitree_legged_real/src/exe/ros_udp.cpp
+++ unitree_legged_real/src/exe/ros_udp.cpp
@@ -8,6 +8,8 @@
 #include <chrono>
 #include <pthread.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Imu.h>
+#include <nav_msgs/Odometry.h>
 
 using namespace UNITREE_LEGGED_SDK;
 class Custom
@@ -71,6 +73,8 @@ ros::Subscriber sub_cmd_vel;
 ros::Publisher pub_high;
 ros::Publisher pub_low;
 ros::Publisher pub_cmd_vel;
+ros::Publisher pub_imu;
+ros::Publisher pub_odom;
 
 long high_count = 0;
 long low_count = 0;
@@ -138,6 +142,55 @@ void timerCallback(const ros::TimerEvent&)
     unitree_legged_msgs::HighState high_state_ros;
     high_state_ros = state2rosMsg(custom.high_state);
     pub_high.publish(high_state_ros);
+
+    // compute imu from high_state lcm msg
+    ros::Time current_time;
+
+    current_time = ros::Time::now();
+
+    sensor_msgs::Imu msg_imu;
+
+    msg_imu.header.seq = timer_count;
+    msg_imu.header.stamp = current_time;
+    msg_imu.header.frame_id = "imu_link";
+
+    msg_imu.orientation.w = custom.high_state.imu.quaternion[0];
+    msg_imu.orientation.x = custom.high_state.imu.quaternion[1];
+    msg_imu.orientation.y = custom.high_state.imu.quaternion[2];
+    msg_imu.orientation.z = custom.high_state.imu.quaternion[3];
+
+    msg_imu.angular_velocity.x = custom.high_state.imu.gyroscope[0];
+    msg_imu.angular_velocity.y = custom.high_state.imu.gyroscope[1];
+    msg_imu.angular_velocity.z = custom.high_state.imu.gyroscope[2];
+
+    msg_imu.linear_acceleration.x = custom.high_state.imu.accelerometer[0];
+    msg_imu.linear_acceleration.y = custom.high_state.imu.accelerometer[1];
+    msg_imu.linear_acceleration.z = custom.high_state.imu.accelerometer[2];
+
+    pub_imu.publish(msg_imu);
+
+    // compute odometry from high_state lcm msg
+    nav_msgs::Odometry msg_odom;
+
+    msg_odom.header.seq = timer_count;
+    msg_odom.header.stamp = current_time;
+    msg_odom.header.frame_id = "odom";
+    msg_odom.header.frame_id = "base_link";
+
+    msg_odom.pose.pose.position.x = custom.high_state.position[0];
+    msg_odom.pose.pose.position.y = custom.high_state.position[1];
+    msg_odom.pose.pose.position.z = custom.high_state.position[2];
+
+    msg_odom.pose.pose.orientation.w = custom.high_state.imu.quaternion[0];
+    msg_odom.pose.pose.orientation.x = custom.high_state.imu.quaternion[1];
+    msg_odom.pose.pose.orientation.y = custom.high_state.imu.quaternion[2];
+    msg_odom.pose.pose.orientation.z = custom.high_state.imu.quaternion[3];
+
+    msg_odom.twist.twist.linear.x = custom.high_state.velocity[0];
+    msg_odom.twist.twist.linear.y = custom.high_state.velocity[1];
+    msg_odom.twist.twist.angular.z = custom.high_state.velocity[2];
+
+    pub_odom.publish(msg_odom);
 
     //printf("timerCallback\t%ld\n\n", timer_count++);
 }
